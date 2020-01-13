1d7ae12db3a24483c135f312e9464d74cae72582
lpoznic <lpoznic@net.hr>
2020-01-13 14:55:46 +0100

    Valjda

--- src/Edunova/Zadatak05.java
+++ src/Edunova/Zadatak05.java
@@ -5,18 +5,57 @@ import javax.swing.JOptionPane;
 public class Zadatak05 {
 
 	public static void main(String[] args) {
-	
-		int a=Integer.parseInt(JOptionPane.showInputDialog("Upišite broj 1"));
-		int b=Integer.parseInt(JOptionPane.showInputDialog("Upišite broj 2"));
-		int poc=1;
-		int[][] mat= new int [a][b];
 		
-		for(int i=a; i>0 ;i--) {
+
+		int [] [] matrica=new int [5][5];
+		
+		for (int i=0;i<5;i++) {
+			for (int j=0;j<5;j++) {  
+				
+				matrica[i][j]=Integer.parseInt(JOptionPane.showInputDialog("Unesite elemente u 1. matricu " + (i+1) + " " + (j+1)));
+
+				
+				System.out.print(matrica[i][j] + " ");
+			}
+			System.out.println("");
+			}
+
+	        int stupac1 = 0;
+	        int red1 = 0;
+	        int stupac2= 4;
+	        int red2 = 4;
+	        int i = 0;
+	        int j = 0;
+	        int br=1;
+	        
+	        while(br <= 4 * 4){
+	        	
+	        	 for(i = red2 ; i >= red1; i--)
+		                System.out.println(matrica[stupac2][i]); 
+		                 br++;
+		             for(i = stupac2 - 1; i >= stupac1 + 1; i--)
+		               System.out.println(matrica[i][red1] ); 
+		                 br++;
+	        	
+	        	 for(i = red1; i <= red2; i++)
+	                 System.out.println(matrica[stupac1][i]);
+	                 br++;
+	             for(i = stupac1 + 1; i <= stupac2 - 1 ; i++)
+	                 System.out.println(matrica[i][red2]);
+	                 br++;
+	            
+	                 
+	             red1++;
+	             red2--;
+	             stupac1++;
+	             stupac2--;
+	            
+	            
+	           
+	        }
 			
-			mat[i][b]=poc++;
-			System.out.println(mat[i][b]);
-			
-		}
+	
 		
 	}
+
 }
