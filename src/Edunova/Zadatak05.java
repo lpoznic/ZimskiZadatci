56c45da4a3787269637ebad363f49f91de2c3b6e
lpoznic <lpoznic@net.hr>
2020-01-16 21:36:18 +0100

    Rijesen 5. zad

--- src/Edunova/Zadatak05.java
+++ src/Edunova/Zadatak05.java
@@ -5,57 +5,58 @@ import javax.swing.JOptionPane;
 public class Zadatak05 {
 
 	public static void main(String[] args) {
-		
+		 
+		int a=Integer.parseInt(JOptionPane.showInputDialog("Upišite 1. br"));
+		 int b=Integer.parseInt(JOptionPane.showInputDialog("Upišite 2. br"));
+		 
+		 //r-max red, s-max stupac, mins-min stupac, minr-min red
+		 int r=b, s=a, poc=1, mins=0, minr=0;
+		 int [][] mat=new int [a][b];
 
-		int [] [] matrica=new int [5][5];
-		
-		for (int i=0;i<5;i++) {
-			for (int j=0;j<5;j++) {  
-				
-				matrica[i][j]=Integer.parseInt(JOptionPane.showInputDialog("Unesite elemente u 1. matricu " + (i+1) + " " + (j+1)));
+		 while(true){
+			 
+			 if(mins>s) {
+				 break;
+			 }
 
-				
-				System.out.print(matrica[i][j] + " ");
-			}
-			System.out.println("");
-			}
+		 for(int i=s-1;i>=mins;i--){
+			 mat[i][r-1]=poc++;
+		 }
+		 r--;
+		 
+		 if(minr>r) {
+			 break;
+		 }
 
-	        int stupac1 = 0;
-	        int red1 = 0;
-	        int stupac2= 4;
-	        int red2 = 4;
-	        int i = 0;
-	        int j = 0;
-	        int br=1;
-	        
-	        while(br <= 4 * 4){
-	        	
-	        	 for(i = red2 ; i >= red1; i--)
-		                System.out.println(matrica[stupac2][i]); 
-		                 br++;
-		             for(i = stupac2 - 1; i >= stupac1 + 1; i--)
-		               System.out.println(matrica[i][red1] ); 
-		                 br++;
-	        	
-	        	 for(i = red1; i <= red2; i++)
-	                 System.out.println(matrica[stupac1][i]);
-	                 br++;
-	             for(i = stupac1 + 1; i <= stupac2 - 1 ; i++)
-	                 System.out.println(matrica[i][red2]);
-	                 br++;
-	            
-	                 
-	             red1++;
-	             red2--;
-	             stupac1++;
-	             stupac2--;
-	            
-	            
-	           
-	        }
-			
-	
-		
-	}
+		 for(int i=r-1;i>=minr;i--){
+			 mat[mins][i]=poc++;
+		 }
+		 mins++;
+		 
+		 if(mins>s) {
+			 break;
+		 }
 
+		 for(int i=mins;i<s;i++){
+			 mat[i][minr]=poc++;
+		 }
+		 minr++;
+		 
+		 if(minr>r) {
+			 break;
+		 }
+		 
+		 for(int i=minr;i<r;i++){
+			 mat[s-1][i]=poc++;
+
+		 }
+		 s--;
+		 }
+		 for(int i=0;i<b;i++) {
+			 for(int j=0;j<a;j++) {
+				 System.out.print(mat[j][i] + " ");
+			 }
+			 System.out.println();
+		 }
+		 }
 }
